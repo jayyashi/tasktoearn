6a73c83bc17ec36af5acaa93bc8e064d4eb0551a
jayyashi <jay.talentpartners@gmail.com>
2025-03-04 18:54:11 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,18 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx';
+import MasterReport from './MasterReport.tsx';
 import './index.css';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+        <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/master-report" element={<MasterReport />} />
+        <Route path="/:memberId" element={<App />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>
 );
